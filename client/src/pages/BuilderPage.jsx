0d2be9602773094329f440aa152a2a6f69b9eb70
Ayush <anshumanayush208@gmail.com>
2026-08-11 23:30:51 +0530

    feat: implement BuilderHeader component with project controls and navigation

--- client/src/pages/BuilderPage.jsx
+++ client/src/pages/BuilderPage.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useAppContext } from '../context/AppContext';
+import { Loading } from "../components/Loading" 
 
 const BuilderPage = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+  const [leftTab, setLeftTab] = useState("chat");
+  const [publishing, setPublishing] = useState(false);
+  const [publishUrl, setPublishUrl] = useState(null);
+
+  const {activeProject, loadingActiveProject, activeFile, showCode, setActiveFile, setShowCode, loadProject, logout} = useAppContext();
+
+  useEffect(() => {
+    if (!id) return;
+    loadProject(id)
+  }, [id, loadProject])
+
+  useEffect(() => {
+    if (!id || !activeProject) return;
+    if (activeProject.status === "pending" || activeProject.status === "generating") {
+      const interval = setInterval(() => {
+        loadProject(id, true)
+      }, 1500)
+      return () => clearInterval(interval)
+    }
+  }, [id, loadProject, activeProject])
+
+  if(loadingActiveProject || !activeProject) {
+    return <Loading />
+  }
+
   return (
-    <div>
-      Builder
+    <div className="h-screen flex flex-col bg-white overflow-hidden text-zinc-900 relative">
+      {/* Top Bar Header  */}
+
+      {/* Main Layout */}
     </div>
   )
 }
